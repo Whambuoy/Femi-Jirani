be5c29e37e673be934296d943617ed89e38634ba
Whambuoy <esther25maina@gmail.com>
2018-11-02 22:52:51 +0300

    With Welcome Activity

--- app/src/main/java/com/example/android/femijirani/MainActivity.java
+++ app/src/main/java/com/example/android/femijirani/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.android.femijirani;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonLogin = (Button) findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+
+    }
+        public void openNewActivity(){
+            Intent intent = new Intent(this, Welcome.class);
+            startActivity(intent);
+        }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
